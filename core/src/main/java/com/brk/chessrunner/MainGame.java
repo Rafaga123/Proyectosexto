9d7b1572556707bfb34851085e7b5c4436489e97
Rafael Telles <rafael.telles1@hotmail.com>
2026-04-26 22:43:59 -0400

    Testeo de generacion de piezas

--- core/src/main/java/com/brk/chessrunner/MainGame.java
+++ core/src/main/java/com/brk/chessrunner/MainGame.java
@@ -1,32 +1,76 @@
 package com.brk.chessrunner;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MainGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    SpriteBatch batch;
+    Texture atlasTextura;
+    TextureRegion piezaRey;
+
+    // --- NUEVAS VARIABLES PARA RESPONSIVE ---
+    OrthographicCamera camera;
+    Viewport viewport;
+
+    // Definimos nuestra resolución virtual (siempre trabajaremos sobre esto)
+    final float WORLD_WIDTH = 480;
+    final float WORLD_HEIGHT = 800;
+    // -----------------------------------------
+
+    float x, y;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        atlasTextura = new Texture("atlas.png");
+        piezaRey = new TextureRegion(atlasTextura, 16, 208, 16, 16);
+
+        // 1. Configuramos la cámara
+        camera = new OrthographicCamera();
+
+        // 2. Configuramos el Viewport con el tamaño virtual
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        // 3. Centramos la cámara al inicio
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+
+        // Ahora nuestras coordenadas son respecto a 480x800, no píxeles reales
+        x = WORLD_WIDTH / 2 - 32;
+        y = 50;
     }
 
     @Override
     public void render() {
+        // Actualizamos la cámara
+        camera.update();
+
+        // Le decimos al Batch que use la vista de la cámara
+        batch.setProjectionMatrix(camera.combined);
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(piezaRey, x, y, 64, 64);
         batch.end();
+    }
+
+    // --- ESTE METODO ES VITAL PARA EL RESPONSIVE ---
+    @Override
+    public void resize(int width, int height) {
+        // Cuando la ventana cambia de tamaño, el viewport ajusta el mundo virtual
+        viewport.update(width, height);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        atlasTextura.dispose();
     }
 }
